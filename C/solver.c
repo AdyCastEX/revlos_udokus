c86cd7d315f78deaafb2960c38a496dee05133fb
Inna Castaneda <alexacastaneda14@gmail.com>
2015-05-13 20:34:03 +0800

    checker x, y, xy

--- C/solver.c
+++ C/solver.c
@@ -11,13 +11,16 @@ main(){
 	int num_puzzles;
 	int i,j;
 
-	char filename[] = "puzzle3.in";
+	char filename[] = "puzzle6.in";
 
 	boards = createBoard(&num_puzzles,&subgrids,filename);
 
 	for(i=0;i<num_puzzles;i+=1){	
 		//printGrid(boards[i],subgrids[i]*subgrids[i]);
-		printf("number of solutions: %d\n",solve_puzzle(boards[i],subgrids[i]));
+		printf("number of regular solutions: %d\n",solve_puzzle(boards[i],subgrids[i], 0));
+		printf("number of X solutions: %d\n",solve_puzzle(boards[i],subgrids[i], 1));
+		printf("number of Y solutions: %d\n",solve_puzzle(boards[i],subgrids[i], 2));
+		printf("number of XY solutions: %d\n",solve_puzzle(boards[i],subgrids[i], 3));
 	}
 	
 }
